5f06bbf51b1ae194b88c6559a1716fb161ecb791
Aurben <50946276+Aurben@users.noreply.github.com>
2019-05-31 20:29:01 +0300

    Add files via upload

---
+++ Program cs Files/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jacks_or_Better
+{
+    class Program
+    {
+        /*enum Suites
+        {
+            Hearts= '♥',
+            Diamonds = '♦',
+            Clubs = '♣',
+            Spades = '♠'
+        }*/
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8; // UTF8 is needed for displaying card suits
+            GameManager game = new GameManager();
+            bool play = true;
+
+            Console.WriteLine("Lets play some \"Jacks or better\" poker.\n");
+            while (play)
+            {
+                game.StartGame();
+                string playerAnswer = "";
+
+                //Ask for input until it is either y or n.
+                while (playerAnswer.ToLower() != "y" && playerAnswer.ToLower() != "n")
+                {
+                    Console.WriteLine("\nPlay again? (Y/N)");
+                    playerAnswer = Console.ReadLine();
+                    if (playerAnswer.ToLower() == "n")
+                        play = false;
+
+                }
+            }
+            Console.WriteLine("Thank you for playing.");
+        }
+    }
+}
